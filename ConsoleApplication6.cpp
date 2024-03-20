a100b6000460712685b307919a7ff0b68e8d111b
Vytvyk <153222587+Vytvyk@users.noreply.github.com>
2024-03-20 09:55:14 +0200

    Add files via upload

---
+++ ConsoleApplication6.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    // Перевіряємо всі чотиризначні натуральні числа
+    for (int i = 1000; i <= 9999; i++) {
+        // Знаходимо суму цифр числа
+        int sum = 0;
+        while (i > 0) {
+            sum += i % 10;
+            i /= 10;
+        }
+
+        // Якщо куб суми цифр дорівнює числу, то виводимо його на екран
+        if (pow(sum, 3) == i) {
+            cout << i << endl;
+            return 0;
+        }
+    }
+
+    // Якщо жодного числа не знайдено, то виводимо повідомлення про це
+    cout << "this number does not exist" << endl;
+    
+    return 0;
+}
